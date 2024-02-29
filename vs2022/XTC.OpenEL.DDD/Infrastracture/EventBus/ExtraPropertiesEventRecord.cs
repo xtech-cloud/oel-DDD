76385a61a5aad4997619e1455f02dffe11781349
Niall Liu <easlee@outlook.com>
2024-02-29 14:09:31 +0800

    add ExtraPropertiesEventRecord

---
+++ vs2022/XTC.OpenEL.DDD/Infrastracture/EventBus/ExtraPropertiesEventRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace XTC.OpenEL.DDD.Infrastracture.EventBus;
+
+public class ExtraPropertiesEventRecord : IEventRecord
+{
+    public IEventData Data { get; private set; }
+
+    public ExtraPropertiesEventRecord(ExtraPropertiesEventData _data)
+    {
+        Data = _data;
+    }
+}
